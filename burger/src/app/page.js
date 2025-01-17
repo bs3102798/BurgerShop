0e9520f6dfbf23305af1a82cb155dc861098237e
Burness <bsiii102798@gmail.com>
2025-01-17 10:21:29 -0800

    header-component

---
+++ burger/src/app/page.js
@@ -0,0 +1,13 @@
+import Header from "@/components/layout/Header";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+    <Header />
+    
+
+    </>
+  );
+}
