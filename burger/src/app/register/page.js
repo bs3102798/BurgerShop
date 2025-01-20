1c3f9d78be7329af267aa3e6a13f8202e7b0d6d1
Burness <bsiii102798@gmail.com>
2025-01-19 17:26:17 -0800

    created&alreadyCreatedUsers

--- burger/src/app/register/page.js
+++ burger/src/app/register/page.js
@@ -2,21 +2,40 @@
 'use client'
 
 import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 
 export default function RegisterPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    function handleFormSubmit(ev) {
+    const [creatingUser, setCreatedingUser] = useState(false)
+    const [userCreated, setUserCreated] = useState(false)
+    const [error, setError] = useState(false)
+    async function handleFormSubmit(ev) {
+
         ev.preventDefault();
-        fetch('/api/register',
+        setCreatedingUser(true)
+        setError(false);
+        setUserCreated(false)
+
+
+        const response = await fetch('/api/register',
             {
                 method: 'POST',
                 body: JSON.stringify({ email, password }),
-                headers: {'Content-Type': 'application/json'},
-            },
+                headers: { 'Content-Type': 'application/json' },
+            });
+        if (response.ok) {
+            setUserCreated(true);
+        }
+        else {
+            setError(true);
+        }
 
-        )
+        setCreatedingUser(false)
+
+
+
 
     }
     return (
@@ -24,19 +43,37 @@ export default function RegisterPage() {
             <h1 className="text-center text-primary text-4xl">
                 Register
             </h1>
+            {userCreated && (
+                <div className="my-4 text-center">
+                    User Created.  <br />Now you can <Link href={'/login'}>Login &raquo; </Link>
+                </div>
+            )}
+            {error && (
+                <div className="my-4 text-center">
+                    User Allready Created.  <br />Please try again later or create new user
+                </div>
+
+            )}
             <form className="block max-w-lg mx-auto" onSubmit={handleFormSubmit}>
                 <input
                     type="email"
                     placeholder="email"
                     value={email}
+                    disabled={creatingUser}
                     onChange={ev => setEmail(ev.target.value)}
                 />
                 <input
                     type="password"
                     placeholder="password"
+                    disabled={creatingUser}
                     value={password}
                     onChange={ev => setPassword(ev.target.value)} />
-                <button type="submit">Register</button>
+                <button
+                    type="submit"
+                    disabled={creatingUser}
+                >
+                    Register
+                </button>
                 <p className="text-center my-4 text-grey-400">
                     or Login with provider
                 </p>
