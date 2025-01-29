022fe0be97e7b5eebd4f48ae01b5f8a3feaec13e
Burness <bsiii102798@gmail.com>
2025-01-29 12:52:38 -0800

    MenuItem/pop-up/priceUpdates

--- burger/src/components/menu/MenuItem.js
+++ burger/src/components/menu/MenuItem.js
@@ -11,23 +11,31 @@ export default function MenuItem(menuItem) {
         sizes, extraTopPrices,
     } = menuItem
     const { AddToCart } = useContext(CartContext)
-    const [selectedSize, setSelectedSize] = useState(null)
+    const [
+        selectedSize, setSelectedSize
+    ] = useState(sizes?.[0] || null)
     const [selectedExtras, setSelectedExtras] = useState([])
     const [showPopup, setShowPopup] = useState(false)
+
     function handleAddToCartButtonClick() {
-        if (sizes.length === 0 && extraTopPrices.length === 0) {
-            AddToCart(menuItem);
-            toast.success('added to cart')
-        } else {
+        const hasOptions = sizes.length > 0 && extraTopPrices.length > 0
+        if (hasOptions) {
             setShowPopup(true)
-
+        } else {
+            if(showPopup) {
+                AddToCart(menuItem, selectedSize, selectedExtras)
+            } else{
+                AddToCart(menuItem);
+            }
+            setShowPopup(false)
+            toast.success('added to cart')
         }
-
     }
+    
     function handleExtraThing(ev, extraTopPrices) {
         //console.log(ev)
         const checked = ev.target.checked
-        if(checked) {
+        if (checked) {
             setSelectedExtras(prev => [...prev, extraTopPrices])
         } else {
             setSelectedExtras(prev => {
@@ -35,11 +43,26 @@ export default function MenuItem(menuItem) {
             })
         }
     }
+    let selectedPrice = basePrice
+
+    if (selectedSize) {
+        selectedPrice += selectedSize.price;
+    }
+
+    if (selectedExtras?.length > 0) {
+        for (const extra of selectedExtras) {
+            selectedPrice += extra.price;
+        }
+    }
     return (
         <>
             {showPopup && (
-                <div className="fixed inset-0 bg-black/80 flex items-center justify-center">
-                    <div className=" my-8 bg-white p-3 rounded-lg max-w-md max-h-screen ">
+                <div
+                    onClick={() => setShowPopup(false)}
+                    className="fixed inset-0 bg-black/80 flex items-center justify-center">
+                    <div
+                        onClick={(ev) => ev.stopPropagation()}
+                        className=" my-8 bg-white p-3 rounded-lg max-w-md max-h-screen ">
                         <div className="overflow-y-scroll p-2"
                             style={{ maxHeight: 'calc(100vh - 100px' }}>
 
@@ -54,11 +77,11 @@ export default function MenuItem(menuItem) {
                                     <h3 className="text-center text-gray-700">Pick your size</h3>
                                     {sizes.map(size => (
                                         <label className="flex items-center gap-3 p-4 border rounded-md mb-1">
-                                            <input 
-                                            onChange={() => setSelectedSize(sizes)}
-                                            checked={selectedSize?.name === sizes.name}
-                                            type="radio" 
-                                            name="size" />{size.name} ${basePrice + size.price}
+                                            <input
+                                                onChange={() => setSelectedSize(size)}
+                                                checked={selectedSize?.name === size.name}
+                                                type="radio"
+                                                name="size" />{size.name} ${basePrice + size.price}
                                         </label>
                                     ))}
                                 </div>
@@ -69,16 +92,22 @@ export default function MenuItem(menuItem) {
                                     {JSON.stringify(selectedExtras)}
                                     {extraTopPrices.map(extraTopPrices => (
                                         <label className="flex items-center gap-3 p-4 border rounded-md mb-1">
-                                            <input 
-                                            onClick={ev => handleExtraThing(ev,extraTopPrices)}
-                                            type="checkbox" 
-                                            name={extraTopPrices} />{extraTopPrices.name}
+                                            <input
+                                                onClick={ev => handleExtraThing(ev, extraTopPrices)}
+                                                type="checkbox"
+                                                name={extraTopPrices} />{extraTopPrices.name}
                                             ${extraTopPrices.price}
                                         </label>
                                     ))}
                                 </div>
                             )}
-                            <button className="primary" type="button">Add to chart selected price</button>
+                            <button
+                                className="primary sticky bottom-2"
+                                onClick={handleAddToCartButtonClick}
+                                type="button">
+                                Add to chart ${selectedPrice}
+                            </button>
+                            <button className="mt-2" onClick={() => setShowPopup(false)}>Cancel</button>
 
                         </div>
                     </div>
--- burger/src/components/menu/MenuItemTile.js
+++ burger/src/components/menu/MenuItemTile.js
@@ -1,5 +1,5 @@
 export default function MenuItemTile({ onAddToCart,...item}) {
-    const {image, description, name, basePrice,} = item;
+    const {image, description, name, basePrice, sizes, extraTopPrices} = item;
     return(
         <div className="bg-gray-300 p-4 rounded-lg text-center hover:bg-gray-200 hover:shadow-md hover:shadow-black/25">
                 <div className="text-center">
@@ -14,7 +14,12 @@ export default function MenuItemTile({ onAddToCart,...item}) {
                     type="button"
                     onClick={onAddToCart}
                     className="bg-primary text-white rounded-full px-8 py-2 mt-4">
-                    Add to cart ${basePrice}
+                        {(sizes?.length > 0 || extraTopPrices?.length > 0) ? (
+                            <span>starting from ${basePrice}</span>
+                        ): (
+                            <span>Add to cart ${basePrice}</span>
+                        )}
+                    {/* Add to cart ${basePrice} */}
                 </button>
             </div>
 
