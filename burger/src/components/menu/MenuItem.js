5b9c42f0af6dd003dafd265efa469a5dc9a56957
Burness <bsiii102798@gmail.com>
2025-01-28 18:32:50 -0800

    itempopup/extraToppings

--- burger/src/components/menu/MenuItem.js
+++ burger/src/components/menu/MenuItem.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-key */
 import { useContext, useState } from "react"
 import { CartContext } from "../AppContext"
 import toast from "react-hot-toast"
@@ -10,6 +11,8 @@ export default function MenuItem(menuItem) {
         sizes, extraTopPrices,
     } = menuItem
     const { AddToCart } = useContext(CartContext)
+    const [selectedSize, setSelectedSize] = useState(null)
+    const [selectedExtras, setSelectedExtras] = useState([])
     const [showPopup, setShowPopup] = useState(false)
     function handleAddToCartButtonClick() {
         if (sizes.length === 0 && extraTopPrices.length === 0) {
@@ -21,28 +24,63 @@ export default function MenuItem(menuItem) {
         }
 
     }
+    function handleExtraThing(ev, extraTopPrices) {
+        //console.log(ev)
+        const checked = ev.target.checked
+        if(checked) {
+            setSelectedExtras(prev => [...prev, extraTopPrices])
+        } else {
+            setSelectedExtras(prev => {
+                return prev.filter(e => e.name !== extraTopPrices.name)
+            })
+        }
+    }
     return (
         <>
             {showPopup && (
                 <div className="fixed inset-0 bg-black/80 flex items-center justify-center">
-                    <div className="bg-white p-4 rounded-lg max-w-md">
-                        <Image
-                            src={image} alt={name}
-                            width={200} height={200}
-                            className="mx-auto" />
-                        <h2 className="text-lg bold text-center mb-3">{name}</h2>
-                        <p className="text-center text-gray-600 text-am mb-3">{description}</p>
-                        {sizes?.length > 0 && (
-                            <div className="bg-gray-200 rounded-md p-2">
-                                <h3>Pick your size</h3>
-                                {sizes.map(size => (
-                                    <label>
-                                        <input type="radio" />{size.name} {size.price}
-                                    </label>
-                                ))}
-                            </div>
-                        )}
+                    <div className=" my-8 bg-white p-3 rounded-lg max-w-md max-h-screen ">
+                        <div className="overflow-y-scroll p-2"
+                            style={{ maxHeight: 'calc(100vh - 100px' }}>
 
+                            <Image
+                                src={image} alt={name}
+                                width={200} height={200}
+                                className="mx-auto" />
+                            <h2 className="text-lg bold text-center mb-3">{name}</h2>
+                            <p className="text-center text-gray-600 text-am mb-3">{description}</p>
+                            {sizes?.length > 0 && (
+                                <div className="  py-2">
+                                    <h3 className="text-center text-gray-700">Pick your size</h3>
+                                    {sizes.map(size => (
+                                        <label className="flex items-center gap-3 p-4 border rounded-md mb-1">
+                                            <input 
+                                            onChange={() => setSelectedSize(sizes)}
+                                            checked={selectedSize?.name === sizes.name}
+                                            type="radio" 
+                                            name="size" />{size.name} ${basePrice + size.price}
+                                        </label>
+                                    ))}
+                                </div>
+                            )}
+                            {extraTopPrices?.length > 0 && (
+                                <div className="py-2">
+                                    <h3 className="text-center text-gray-700">Extra Topping?</h3>
+                                    {JSON.stringify(selectedExtras)}
+                                    {extraTopPrices.map(extraTopPrices => (
+                                        <label className="flex items-center gap-3 p-4 border rounded-md mb-1">
+                                            <input 
+                                            onClick={ev => handleExtraThing(ev,extraTopPrices)}
+                                            type="checkbox" 
+                                            name={extraTopPrices} />{extraTopPrices.name}
+                                            ${extraTopPrices.price}
+                                        </label>
+                                    ))}
+                                </div>
+                            )}
+                            <button className="primary" type="button">Add to chart selected price</button>
+
+                        </div>
                     </div>
                 </div>
             )}
